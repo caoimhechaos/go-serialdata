f2a63aef0f088c076ef6929abfc61fd8ce65d761
Caoimhe Chaos <caoimhechaos@protonmail.com>
2016-11-06 03:27:57 +0100

    Add a small documentation string to the package to describe it properly.

---
+++ doc.go
@@ -0,0 +1,4 @@
+// The serialdata package implements readers and writers for writing and
+// accessing data which is structured into lists of records in files or other
+// Reader/Writer type data structures.
+package serialdata
